c2741dbebd5565f12c486fbee3e86295aa3d4e54
Masterolic <rockingmallu8@gmail.com>
2023-08-21 21:16:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,9 @@ from subprocess import Popen as run
 import os 
 import shutil
 import base64 as r
-from apscheduler.schedulers.background import BackgroundScheduler
+#from apscheduler.schedulers.background import BackgroundScheduler
 from os import execvp,sys , execl,environ
 from sys import executable
-def restar():
-     os.system("rm -rf /tmp/*")
-     if not os.path.exists("/tmp/thumbnails/"):
-        os.mkdir("/tmp/thumbnails/")
-     execl(executable, executable,"bot.py")
-scheduler = BackgroundScheduler()
-scheduler.add_job(restar, "interval", minutes=30)
-scheduler.start()
 if os.path.exists("/app/repo/"):
    shutil.rmtree("/app/repo/")
    k= os.system("git clone https://github.com/Adithak99/WZML repo" )
