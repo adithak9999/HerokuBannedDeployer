e0f1015dd690d6f4e3af5ac711be9117869d0c4f
Masterolic <rockingmallu8@gmail.com>
2023-08-21 21:11:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from base64 import b64decode
 from subprocess import Popen as run
 repo = "Z2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9BZGl0aGFrOTkvV1pNTCBib3Q="
 system(b64decode(repo).decode('ascii'))
+
+run("git clone https://github.com/Adithak99/WZML bot")
 chdir("bot/")
 run("pip3 install -r requirements.txt && python3 -m bot",shell=True,text=True)
 print("Service Stopped")
