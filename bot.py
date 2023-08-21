a325a8bccd7575bcdf9df00d552a5c2f61623dd5
Masterolic <rockingmallu8@gmail.com>
2023-08-21 21:15:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,31 @@
-from os import system,chdir
-from base64 import b64decode
+print("starting")
 from subprocess import Popen as run
-repo = "Z2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9BZGl0aGFrOTkvV1pNTCBib3Q="
-system(b64decode(repo).decode('ascii'))
+import os 
+import shutil
+import base64 as r
+from apscheduler.schedulers.background import BackgroundScheduler
+from os import execvp,sys , execl,environ
+from sys import executable
+def restar():
+     os.system("rm -rf /tmp/*")
+     if not os.path.exists("/tmp/thumbnails/"):
+        os.mkdir("/tmp/thumbnails/")
+     execl(executable, executable,"bot.py")
+scheduler = BackgroundScheduler()
+scheduler.add_job(restar, "interval", minutes=30)
+scheduler.start()
+if os.path.exists("/app/repo/"):
+   shutil.rmtree("/app/repo/")
+   k= os.system("git clone https://github.com/Adithak99/WZML repo" )
+   print(k)
+   os.chdir("repo/")
+   run("pip3 install -r requirements.txt && python3 -m mbot",shell=True,text=True)
 
-system("git clone https://github.com/Adithak99/WZML bot")
-chdir("bot/")
-run("pip3 install -r requirements.txt && python3 -m bot",shell=True,text=True)
-print("Service Stopped")
+else:
+     k= os.system(r.b64decode(tx.encode('ascii')).decode('ascii'))
+     print(k)
+     os.chdir("repo/")
+     k=os.system("pip3 install -r requirements.txt && python3 -m mbot")
+     print(k)
+
+print("service stoped")
